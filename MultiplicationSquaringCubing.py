5efaf4be8d6f3ebe96d17bc7fcdcd05a502d718e
FarzanCR7 <86564840+FarzanCR7@users.noreply.github.com>
2021-08-12 07:57:32 -0700

    Add files via upload

---
+++ MultiplicationSquaringCubing.py
@@ -0,0 +1,10 @@
+number = int(input(" Enter the number you would like to get the table of : "))    
+print("The Table of : ",number)  
+for i in range(1,11):    
+   print(number,'x',i,'=',number*i)  
+#%%
+S = int(input("Enter a value you want to get the square of:"))
+print("The squared value is:", S*S)
+#%%
+C = int(input("Enter a value you want to get the cube of:"))
+print("The cubed value is:", C*C*C)
